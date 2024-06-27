cf678280e889ca11bfdd57e9d2f9678f1b9d7953
msamgan <mohdsamgankhan@gmail.com>
2024-06-27 12:31:28 -0400

    added umami.

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require('/Users/msamgan/Projects/boilerplate/x-docs/node_modules/.pnpm/infima@0.2.0-alpha.43/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/msamgan/Projects/boilerplate/x-docs/node_modules/.pnpm/@docusaurus+theme-classic@2.4.1_react-dom@17.0.2_react@17.0.2_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/msamgan/Projects/boilerplate/x-docs/node_modules/.pnpm/@docusaurus+theme-classic@2.4.1_react-dom@17.0.2_react@17.0.2_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/msamgan/Projects/boilerplate/x-docs/src/css/custom.css'),
-];
+    require("/Users/msamgan/Projects/boilerplate/x-docs/node_modules/.pnpm/infima@0.2.0-alpha.43/node_modules/infima/dist/css/default/default.css"),
+    require("/Users/msamgan/Projects/boilerplate/x-docs/node_modules/.pnpm/@docusaurus+theme-classic@2.4.1_react-dom@17.0.2_react@17.0.2_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+    require("/Users/msamgan/Projects/boilerplate/x-docs/node_modules/.pnpm/@docusaurus+theme-classic@2.4.1_react-dom@17.0.2_react@17.0.2_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+    require("/Users/msamgan/Projects/boilerplate/x-docs/src/css/custom.css")
+]
--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -4,367 +4,311 @@
  * Modify the docusaurus.config.js file at your site's root instead.
  */
 export default {
-  "title": "Framework X",
-  "tagline": "Your Accelerated Path to API Creation",
-  "favicon": "img/favicon.ico",
-  "url": "https://frameworkx.info",
-  "baseUrl": "/",
-  "organizationName": "3rd-planet",
-  "projectName": "framework-x",
-  "onBrokenLinks": "throw",
-  "onBrokenMarkdownLinks": "warn",
-  "i18n": {
-    "defaultLocale": "en",
-    "locales": [
-      "en"
-    ],
-    "path": "i18n",
-    "localeConfigs": {}
-  },
-  "presets": [
-    [
-      "classic",
-      {
-        "docs": {
-          "sidebarPath": "/Users/msamgan/Projects/boilerplate/x-docs/sidebars.js",
-          "editUrl": "https://github.com/3rd-planet/x-docs/blob/main/"
-        },
-        "blog": {
-          "showReadingTime": true,
-          "editUrl": "https://github.com/3rd-planet/x-docs/blob/main/"
-        },
-        "theme": {
-          "customCss": "/Users/msamgan/Projects/boilerplate/x-docs/src/css/custom.css"
-        }
-      }
-    ]
-  ],
-  "themeConfig": {
-    "metadata": [
-      {
-        "name": "keywords",
-        "content": "Framework X, API, NodeJs, Express"
-      },
-      {
-        "name": "description",
-        "content": "Framework x is all about substance over style! 3rd Planet's Framework X is like a well-oiled machine for API development, powered by Node.js and Express"
-      }
-    ],
-    "image": "img/frameworkx.png",
-    "navbar": {
-      "title": "Framework X",
-      "logo": {
-        "alt": "Framework X Logo",
-        "src": "img/frameworkx.png"
-      },
-      "items": [
-        {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "position": "left",
-          "label": "Documentation"
-        },
-        {
-          "position": "left",
-          "label": "Contributors",
-          "to": "/contributors"
-        },
-        {
-          "href": "https://github.com/3rd-planet/framework-x",
-          "label": "GitHub",
-          "position": "right"
-        }
-      ],
-      "hideOnScroll": false
+    title: "Framework X",
+    tagline: "Your Accelerated Path to API Creation",
+    favicon: "img/favicon.ico",
+    url: "https://frameworkx.info",
+    baseUrl: "/",
+    organizationName: "3rd-planet",
+    projectName: "framework-x",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+        path: "i18n",
+        localeConfigs: {}
     },
-    "footer": {
-      "style": "dark",
-      "links": [
-        {
-          "title": "Pages",
-          "items": [
+    presets: [
+        [
+            "classic",
             {
-              "label": "Documentation",
-              "to": "/docs/introduction"
+                docs: {
+                    sidebarPath: "/Users/msamgan/Projects/boilerplate/x-docs/sidebars.js",
+                    editUrl: "https://github.com/3rd-planet/x-docs/blob/main/"
+                },
+                blog: {
+                    showReadingTime: true,
+                    editUrl: "https://github.com/3rd-planet/x-docs/blob/main/"
+                },
+                theme: {
+                    customCss: "/Users/msamgan/Projects/boilerplate/x-docs/src/css/custom.css"
+                }
+            }
+        ]
+    ],
+    themeConfig: {
+        metadata: [
+            {
+                name: "keywords",
+                content: "Framework X, API, NodeJs, Express"
             },
             {
-              "label": "Contributors",
-              "to": "/contributors"
+                name: "description",
+                content:
+                    "Framework x is all about substance over style! 3rd Planet's Framework X is like a well-oiled machine for API development, powered by Node.js and Express"
             }
-          ]
+        ],
+        image: "img/frameworkx.png",
+        navbar: {
+            title: "Framework X",
+            logo: {
+                alt: "Framework X Logo",
+                src: "img/frameworkx.png"
+            },
+            items: [
+                {
+                    type: "docSidebar",
+                    sidebarId: "tutorialSidebar",
+                    position: "left",
+                    label: "Documentation"
+                },
+                {
+                    position: "left",
+                    label: "Contributors",
+                    to: "/contributors"
+                },
+                {
+                    href: "https://github.com/3rd-planet/framework-x",
+                    label: "GitHub",
+                    position: "right"
+                }
+            ],
+            hideOnScroll: false
         },
-        {
-          "title": "Community",
-          "items": [
-            {
-              "label": "Discord",
-              "href": "https://discord.gg/H8Cxk2QZ"
-            }
-          ]
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    title: "Pages",
+                    items: [
+                        {
+                            label: "Documentation",
+                            to: "/docs/introduction"
+                        },
+                        {
+                            label: "Contributors",
+                            to: "/contributors"
+                        }
+                    ]
+                },
+                {
+                    title: "Community",
+                    items: [
+                        {
+                            label: "Discord",
+                            href: "https://discord.gg/H8Cxk2QZ"
+                        }
+                    ]
+                },
+                {
+                    title: "More",
+                    items: [
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/3rd-planet/framework-x"
+                        }
+                    ]
+                }
+            ],
+            copyright: "Copyright © 2024 3rd Planet. Built with Docusaurus."
         },
-        {
-          "title": "More",
-          "items": [
-            {
-              "label": "GitHub",
-              "href": "https://github.com/3rd-planet/framework-x"
+        prism: {
+            theme: {
+                plain: {
+                    color: "#393A34",
+                    backgroundColor: "#f6f8fa"
+                },
+                styles: [
+                    {
+                        types: ["comment", "prolog", "doctype", "cdata"],
+                        style: {
+                            color: "#999988",
+                            fontStyle: "italic"
+                        }
+                    },
+                    {
+                        types: ["namespace"],
+                        style: {
+                            opacity: 0.7
+                        }
+                    },
+                    {
+                        types: ["string", "attr-value"],
+                        style: {
+                            color: "#e3116c"
+                        }
+                    },
+                    {
+                        types: ["punctuation", "operator"],
+                        style: {
+                            color: "#393A34"
+                        }
+                    },
+                    {
+                        types: [
+                            "entity",
+                            "url",
+                            "symbol",
+                            "number",
+                            "boolean",
+                            "variable",
+                            "constant",
+                            "property",
+                            "regex",
+                            "inserted"
+                        ],
+                        style: {
+                            color: "#36acaa"
+                        }
+                    },
+                    {
+                        types: ["atrule", "keyword", "attr-name", "selector"],
+                        style: {
+                            color: "#00a4db"
+                        }
+                    },
+                    {
+                        types: ["function", "deleted", "tag"],
+                        style: {
+                            color: "#d73a49"
+                        }
+                    },
+                    {
+                        types: ["function-variable"],
+                        style: {
+                            color: "#6f42c1"
+                        }
+                    },
+                    {
+                        types: ["tag", "selector", "keyword"],
+                        style: {
+                            color: "#00009f"
+                        }
+                    }
+                ]
+            },
+            darkTheme: {
+                plain: {
+                    color: "#F8F8F2",
+                    backgroundColor: "#282A36"
+                },
+                styles: [
+                    {
+                        types: ["prolog", "constant", "builtin"],
+                        style: {
+                            color: "rgb(189, 147, 249)"
+                        }
+                    },
+                    {
+                        types: ["inserted", "function"],
+                        style: {
+                            color: "rgb(80, 250, 123)"
+                        }
+                    },
+                    {
+                        types: ["deleted"],
+                        style: {
+                            color: "rgb(255, 85, 85)"
+                        }
+                    },
+                    {
+                        types: ["changed"],
+                        style: {
+                            color: "rgb(255, 184, 108)"
+                        }
+                    },
+                    {
+                        types: ["punctuation", "symbol"],
+                        style: {
+                            color: "rgb(248, 248, 242)"
+                        }
+                    },
+                    {
+                        types: ["string", "char", "tag", "selector"],
+                        style: {
+                            color: "rgb(255, 121, 198)"
+                        }
+                    },
+                    {
+                        types: ["keyword", "variable"],
+                        style: {
+                            color: "rgb(189, 147, 249)",
+                            fontStyle: "italic"
+                        }
+                    },
+                    {
+                        types: ["comment"],
+                        style: {
+                            color: "rgb(98, 114, 164)"
+                        }
+                    },
+                    {
+                        types: ["attr-name"],
+                        style: {
+                            color: "rgb(241, 250, 140)"
+                        }
+                    }
+                ]
+            },
+            additionalLanguages: [],
+            magicComments: [
+                {
+                    className: "theme-code-block-highlighted-line",
+                    line: "highlight-next-line",
+                    block: {
+                        start: "highlight-start",
+                        end: "highlight-end"
+                    }
+                }
+            ]
+        },
+        algolia: {
+            appId: "ZZR6P6BR18",
+            apiKey: "beaebbbdb3370bca0c9513bf53354837",
+            indexName: "frameworkx",
+            contextualSearch: true,
+            searchParameters: {},
+            searchPagePath: "search"
+        },
+        announcementBar: {
+            id: "support_ukraine",
+            content:
+                'You can support the project by <a target="_blank" rel="noopener noreferrer" href="https://github.com/3rd-planet/framework-x"> Star ⭐ the project on GitHub.</a>',
+            backgroundColor: "#20232a",
+            textColor: "#fff",
+            isCloseable: false
+        },
+        colorMode: {
+            defaultMode: "light",
+            disableSwitch: false,
+            respectPrefersColorScheme: false
+        },
+        docs: {
+            versionPersistence: "localStorage",
+            sidebar: {
+                hideable: false,
+                autoCollapseCategories: false
             }
-          ]
+        },
+        tableOfContents: {
+            minHeadingLevel: 2,
+            maxHeadingLevel: 3
         }
-      ],
-      "copyright": "Copyright © 2024 3rd Planet. Built with Docusaurus."
     },
-    "prism": {
-      "theme": {
-        "plain": {
-          "color": "#393A34",
-          "backgroundColor": "#f6f8fa"
-        },
-        "styles": [
-          {
-            "types": [
-              "comment",
-              "prolog",
-              "doctype",
-              "cdata"
-            ],
-            "style": {
-              "color": "#999988",
-              "fontStyle": "italic"
-            }
-          },
-          {
-            "types": [
-              "namespace"
-            ],
-            "style": {
-              "opacity": 0.7
-            }
-          },
-          {
-            "types": [
-              "string",
-              "attr-value"
-            ],
-            "style": {
-              "color": "#e3116c"
-            }
-          },
-          {
-            "types": [
-              "punctuation",
-              "operator"
-            ],
-            "style": {
-              "color": "#393A34"
-            }
-          },
-          {
-            "types": [
-              "entity",
-              "url",
-              "symbol",
-              "number",
-              "boolean",
-              "variable",
-              "constant",
-              "property",
-              "regex",
-              "inserted"
-            ],
-            "style": {
-              "color": "#36acaa"
-            }
-          },
-          {
-            "types": [
-              "atrule",
-              "keyword",
-              "attr-name",
-              "selector"
-            ],
-            "style": {
-              "color": "#00a4db"
-            }
-          },
-          {
-            "types": [
-              "function",
-              "deleted",
-              "tag"
-            ],
-            "style": {
-              "color": "#d73a49"
-            }
-          },
-          {
-            "types": [
-              "function-variable"
-            ],
-            "style": {
-              "color": "#6f42c1"
-            }
-          },
-          {
-            "types": [
-              "tag",
-              "selector",
-              "keyword"
-            ],
-            "style": {
-              "color": "#00009f"
-            }
-          }
-        ]
-      },
-      "darkTheme": {
-        "plain": {
-          "color": "#F8F8F2",
-          "backgroundColor": "#282A36"
-        },
-        "styles": [
-          {
-            "types": [
-              "prolog",
-              "constant",
-              "builtin"
-            ],
-            "style": {
-              "color": "rgb(189, 147, 249)"
-            }
-          },
-          {
-            "types": [
-              "inserted",
-              "function"
-            ],
-            "style": {
-              "color": "rgb(80, 250, 123)"
-            }
-          },
-          {
-            "types": [
-              "deleted"
-            ],
-            "style": {
-              "color": "rgb(255, 85, 85)"
-            }
-          },
-          {
-            "types": [
-              "changed"
-            ],
-            "style": {
-              "color": "rgb(255, 184, 108)"
-            }
-          },
-          {
-            "types": [
-              "punctuation",
-              "symbol"
-            ],
-            "style": {
-              "color": "rgb(248, 248, 242)"
-            }
-          },
-          {
-            "types": [
-              "string",
-              "char",
-              "tag",
-              "selector"
-            ],
-            "style": {
-              "color": "rgb(255, 121, 198)"
-            }
-          },
-          {
-            "types": [
-              "keyword",
-              "variable"
-            ],
-            "style": {
-              "color": "rgb(189, 147, 249)",
-              "fontStyle": "italic"
-            }
-          },
-          {
-            "types": [
-              "comment"
-            ],
-            "style": {
-              "color": "rgb(98, 114, 164)"
-            }
-          },
-          {
-            "types": [
-              "attr-name"
-            ],
-            "style": {
-              "color": "rgb(241, 250, 140)"
-            }
-          }
-        ]
-      },
-      "additionalLanguages": [],
-      "magicComments": [
-        {
-          "className": "theme-code-block-highlighted-line",
-          "line": "highlight-next-line",
-          "block": {
-            "start": "highlight-start",
-            "end": "highlight-end"
-          }
-        }
-      ]
-    },
-    "algolia": {
-      "appId": "ZZR6P6BR18",
-      "apiKey": "beaebbbdb3370bca0c9513bf53354837",
-      "indexName": "frameworkx",
-      "contextualSearch": true,
-      "searchParameters": {},
-      "searchPagePath": "search"
-    },
-    "announcementBar": {
-      "id": "support_ukraine",
-      "content": "You can support the project by <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/3rd-planet/framework-x\"> Star ⭐ the project on GitHub.</a>",
-      "backgroundColor": "#20232a",
-      "textColor": "#fff",
-      "isCloseable": false
-    },
-    "colorMode": {
-      "defaultMode": "light",
-      "disableSwitch": false,
-      "respectPrefersColorScheme": false
-    },
-    "docs": {
-      "versionPersistence": "localStorage",
-      "sidebar": {
-        "hideable": false,
-        "autoCollapseCategories": false
-      }
-    },
-    "tableOfContents": {
-      "minHeadingLevel": 2,
-      "maxHeadingLevel": 3
+    baseUrlIssueBanner: true,
+    onDuplicateRoutes: "warn",
+    staticDirectories: ["static"],
+    customFields: {},
+    plugins: [],
+    themes: [],
+    scripts: [],
+    headTags: [],
+    stylesheets: [],
+    clientModules: [],
+    titleDelimiter: "|",
+    noIndex: false,
+    markdown: {
+        mermaid: false
     }
-  },
-  "baseUrlIssueBanner": true,
-  "onDuplicateRoutes": "warn",
-  "staticDirectories": [
-    "static"
-  ],
-  "customFields": {},
-  "plugins": [],
-  "themes": [],
-  "scripts": [],
-  "headTags": [],
-  "stylesheets": [],
-  "clientModules": [],
-  "titleDelimiter": "|",
-  "noIndex": false,
-  "markdown": {
-    "mermaid": false
-  }
-};
+}
